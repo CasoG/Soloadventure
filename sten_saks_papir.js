7fc0ef8f46f0e7c8d9e9efdf7f07e31769d15221
CasoG <30808917+CasoG@users.noreply.github.com>
2017-08-09 19:34:46 +0200

    Update sten_saks_papir.js

--- sten_saks_papir.js
+++ sten_saks_papir.js
@@ -5,36 +5,36 @@ function computerChoice() {
 alert(computerChoice);
 
 function playerSten() {
-  if(computerChoice >= 0.66) {
+  if(computerChoice() >= 0.66) {
     alert("Computeren valgte også sten, prøv igen!");
   }
-  else if(computerChoice >=0.33) {
+  else if(computerChoice() >=0.33) {
     alert("Computeren valgte saks, du vandt!");
   }
-  else if(computerChoice >=0) {
+  else if(computerChoice() >=0) {
     alert("Computeren valgte papir, du tabte!");
   }
 }
 
 function playerSaks() {
-  if(computerChoice >= 0.66) {
+  if(computerChoice() >= 0.66) {
     alert("Computeren valgte sten, du tabte!");
   }
-  else if(computerChoice >=0.33) {
+  else if(computerChoice() >=0.33) {
     alert("Computeren valgte også saks, prøv igen!");
   }
-  else if(computerChoice >=0) {
+  else if(computerChoice() >=0) {
     alert("Computeren valgte papir, du vandt!");
   }
 }
 function playerPapir() {
-  if(computerChoice >= 0.66) {
+  if(computerChoice() >= 0.66) {
     alert("Computeren valgte sten, du vandt!");
   }
-  else if(computerChoice >=0.33) {
+  else if(computerChoice() >=0.33) {
     alert("Computeren valgte saks, du tabte!");
   }
-  else if(computerChoice >=0) {
+  else if(computerChoice() >=0) {
     alert("Computeren valgte også papir, prøv igen!");
   }
 }
