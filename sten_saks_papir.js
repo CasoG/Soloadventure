7970182042917cbe88b76fd3a30fa8bad8751d90
CasoG <30808917+CasoG@users.noreply.github.com>
2017-08-09 20:28:43 +0200

    Update sten_saks_papir.js

--- sten_saks_papir.js
+++ sten_saks_papir.js
@@ -2,35 +2,36 @@ var computerChoice = Math.random();
 
 function playerSten() {
   if(computerChoice >= 0.66) {
-    alert("Computeren valgte også sten, prøv igen!");
+    document.getElementById("result").innerHTML = "Computeren valgte også sten, prøv igen!";
   }
   else if(computerChoice >=0.33) {
-    alert("Computeren valgte saks, du vandt!");
+    document.getElementById("result").innerHTML = "Computeren valgte saks, du vandt!";
   }
   else if(computerChoice >=0) {
-    alert("Computeren valgte papir, du tabte!");
+    document.getElementById("result").innerHTML = "Computeren valgte papir, du tabte!";
   }
 }
 
 function playerSaks() {
   if(computerChoice >= 0.66) {
-    alert("Computeren valgte sten, du tabte!");
+    document.getElementById("result").innerHTML = "Computeren valgte sten, du tabte!";
   }
   else if(computerChoice >=0.33) {
-    alert("Computeren valgte også saks, prøv igen!");
+    document.getElementById("result").innerHTML = "Computeren valgte også saks, prøv igen!";
   }
   else if(computerChoice >=0) {
-    alert("Computeren valgte papir, du vandt!");
+    document.getElementById("result").innerHTML = "Computeren valgte papir, du vandt!";
   }
 }
 function playerPapir() {
   if(computerChoice >= 0.66) {
-    alert("Computeren valgte sten, du vandt!");
+    document.getElementById("result").innerHTML = "Computeren valgte sten, du vandt!";
   }
   else if(computerChoice >=0.33) {
-    alert("Computeren valgte saks, du tabte!");
+    document.getElementById("result").innerHTML = "Computeren valgte saks, du tabte!";
   }
   else if(computerChoice >=0) {
-    alert("Computeren valgte også papir, prøv igen!");
+    document.getElementById("result").innerHTML = "Computeren valgte også papir, prøv igen!";
   }
 }
+document.getElementById("result").innerHTML = resultis;
